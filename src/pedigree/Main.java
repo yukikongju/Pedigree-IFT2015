e53d6bb5b29d3196514338eb5dc75cc4f2caec97
Emulie Chhor <emulie.chhor@hotmail.com>
2021-03-09 02:48:08 -0500

    Fixed: can go into Death case

--- src/pedigree/Main.java
+++ src/pedigree/Main.java
@@ -1,61 +1,14 @@
 
 package pedigree;
 
-import java.util.Random;
-
 public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-//       Simulation simulation = new Simulation();
-//       simulation.simulate(5, 2000);
-        
-    // TEST: testing if pq works
-//        PQ pq = new PQ();
-//        AgeModel model = new AgeModel();
-//        Random random = new Random();
-//        for(int i = 0; i<8; i++){
-//            Sim sim = new Sim(Sim.Sex.F);
-//            Event event = new Event(sim, model.randomAge(random), Event.EventType.BIRTH);
-//            pq.insert(event);
-//        }
-//        System.out.println(pq);
-        PQ p = new PQ();
-        Sim sim = new Sim(Sim.Sex.F);
-        Event e1 = new Event(sim, 1, Event.EventType.BIRTH);
-        Event e11 = new Event(sim, 1, Event.EventType.BIRTH);
-        Event e2 = new Event(sim, 2, Event.EventType.BIRTH);
-        Event e22 = new Event(sim, 2, Event.EventType.BIRTH);
-        Event e3 = new Event(sim, 3, Event.EventType.BIRTH);
-        Event e4 = new Event(sim, 4, Event.EventType.BIRTH);
-        Event e5 = new Event(sim, 5, Event.EventType.BIRTH);
-        Event e6 = new Event(sim, 6, Event.EventType.BIRTH);
-        Event e7 = new Event(sim, 7, Event.EventType.BIRTH);
-        Event e8 = new Event(sim, 8, Event.EventType.BIRTH);
-        Event e0 = new Event(sim, 0, Event.EventType.BIRTH);
-
-//                p.insert(e0); // PROB: pq only works with value that are not 0 bc comparison fails otherwise
-
-        p.insert(e5);
-        p.insert(e4);
-        p.insert(e6);
-        p.insert(e1);
-        p.insert(e2);
-        p.insert(e3);
-        p.insert(e22);
-        p.insert(e11);
-        p.insert(e7);
-        p.insert(e8);
-
-//        System.out.println(p);
-        
-        Event removed = (Event) p.deleteMin();
-        System.out.println(removed);
-        
-        System.out.println(p);
-
+       Simulation simulation = new Simulation();
+       simulation.simulate(5, 2000);
     }
 
 }
--- src/pedigree/Simulation.java
+++ src/pedigree/Simulation.java
@@ -15,82 +15,81 @@ import pedigree.Sim.Sex;
  */
 public class Simulation {
     
-//   public final Random RND; 
-//   public final double REPRODUCTION_RATE;
-//    
-//    PQ<Event> eventQ;
-//    AgeModel ageModel;
-//
-//    
-//    //    add ancestors males and females to a hashmap to facilitate mating 
-//    //    HashMap<Sim> aieux;
-//    //    HashMap<Sim> aieules;
-//    //    HashMap<Sim> population;
-//
-//    public Simulation() {
-//        eventQ = new PQ<>();
-//        ageModel = new AgeModel();
-//        RND = new Random();
-//        REPRODUCTION_RATE = 2 / ageModel.expectedParenthoodSpan(Sim.MIN_MATING_AGE_F, Sim.MAX_MATING_AGE_F);
-//    }
-//
-//    public void simulate(int n, double Tmax){
-//        // generate first generation
-//        for(int i = 0; i<n; i++){
-////            Sim fondateur = new Sim(null, null, 0); // founder Sim 
-//            Sim fondateur = new Sim(generateSex(RND)); // TODO: refractor generateSex in SexModel or inside Sim class?
-//            Event E = new Event(fondateur, 0, Event.EventType.BIRTH);
-//            eventQ.insert(E);
-//        }
-//        
-//        // Begin simulation
-//        while(!eventQ.isEmpty()){
-//            Event E = (Event) eventQ.deleteMin();
-//            if(E.getScheduledTime() > Tmax) break; // arrêter à Tmax 
-//            if (E.getSim().getDeathTime() > E.getScheduledTime()){ // TODO: verify if this instruction is necessary
-//                switch (E.getEventType()) { // NOTE: eventType should never be null, so we don't have to check if it's null
-//                    case BIRTH:
-//                        birth(E);
-//                        break;
-//                    case DEATH:
-//                        death(E);
-//                        break;
-//                    case REPRODUCTION:
-//                        reprodution(E);
-//                        break;
-//                    default:
-//                        break;
-//                }
-//            }
-////            System.out.println("YEAR : + " + E.getScheduledTime() + " EVENT TYPE " + E.getEventType().toString());
-//            
-//        }
-//    }
-//    
-//    private Sim.Sex generateSex(Random random){
-//       int temp = random.nextInt(2); // generate 0 or 1
-//       return Sim.Sex.values()[temp];
-//    }
-//
-//    private void birth(Event E) {
-//       double lifeLength = ageModel.randomAge(RND); // lifespan of a Sim
-//       E.getSim().setDeathTime(E.getScheduledTime() + lifeLength);
-//       Event death = new Event(E.getSim(), E.getScheduledTime() + lifeLength, Event.EventType.DEATH);
-//       eventQ.insert(death); // PROBLEM: inserting inside PQ makes the program halt
-//        System.out.println(death);
-//        System.out.println(eventQ);
-//       if (E.getSim().getSex() == Sim.Sex.F){ // TODO: schedule reproduction if sim is a female (how many children does the woman birth)
-////           double ageOfReproduction = AgeModel.randomWaitingTime(RND, REPRODUCTION_RATE);
-////           System.out.println(ageOfReproduction);
-//       }
-//    }
-//
-//    private void death(Event E) {
-//        // TODO: remove sim from population and add it to dead-people
-//        System.out.println("dead");
-//    }
-//
-//    private void reprodution(Event E) {
-//        // TODO: 
-//    }
+   public final Random RND; 
+   public final double REPRODUCTION_RATE;
+    
+    PQ<Event> eventQ;
+    AgeModel ageModel;
+    
+    //    add ancestors males and females to a hashmap to facilitate mating 
+    //    HashMap<Sim> aieux;
+    //    HashMap<Sim> aieules;
+    //    HashMap<Sim> population;
+
+    public Simulation() {
+        eventQ = new PQ<>();
+        ageModel = new AgeModel();
+        RND = new Random();
+        REPRODUCTION_RATE = 2 / ageModel.expectedParenthoodSpan(Sim.MIN_MATING_AGE_F, Sim.MAX_MATING_AGE_F);
+    }
+
+    public void simulate(int n, double Tmax){
+        // generate first generation
+        for(int i = 0; i<n; i++){
+            Sim fondateur = new Sim(generateSex(RND)); // TODO: refractor generateSex in SexModel or inside Sim class?
+            Event E = new Event(fondateur, 0, Event.EventType.BIRTH);
+            eventQ.insert(E);
+        }
+        
+        // Begin simulation
+        while(!eventQ.isEmpty()){
+            Event E = (Event) eventQ.deleteMin();
+            if(E.getScheduledTime() > Tmax) break; // arrêter à Tmax 
+            if (E.getSim().getDeathTime() >= E.getScheduledTime()){ // NOTE: we don't want a strict inequality bc we won't be able to simulate death
+                switch (E.getEventType()) { // NOTE: eventType should never be null, so we don't have to check if it's null
+                    case BIRTH:
+                        birth(E);
+                        break;
+                    case DEATH: // PROBLEME: on ne rentre jamais dans cette boucle (i think it's bc of the if statement)
+                        death(E);
+                        break;
+                    case REPRODUCTION:
+                        reprodution(E);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            System.out.println("YEAR : + " + E.getScheduledTime() + " EVENT TYPE " + E.getEventType().toString());
+            
+        }
+    }
+    
+    private Sim.Sex generateSex(Random random){
+       int temp = random.nextInt(2); // generate 0 or 1
+       return Sim.Sex.values()[temp];
+    }
+
+    private void birth(Event E) {
+       // scheduling death
+       double lifeLength = ageModel.randomAge(RND); // lifespan of a Sim
+       E.getSim().setDeathTime(E.getScheduledTime() + lifeLength);
+       Event death = new Event(E.getSim(), E.getScheduledTime() + lifeLength, Event.EventType.DEATH);
+       eventQ.insert(death); 
+       
+       // scheduling reproduction
+       if (E.getSim().getSex() == Sim.Sex.F){ // TODO: schedule reproduction if sim is a female (how many children does the woman birth)
+//           double ageOfReproduction = AgeModel.randomWaitingTime(RND, REPRODUCTION_RATE);
+//           System.out.println(ageOfReproduction);
+       }
+    }
+
+    private void death(Event E) {
+        // TODO: remove sim from population and add it to dead-people
+        System.out.println("dead");
+    }
+
+    private void reprodution(Event E) {
+        // TODO: 
+    }
 }
