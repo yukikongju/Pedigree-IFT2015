3c2debc926c79fca1461aa284188dada21c99276
Emulie Chhor <emulie.chhor@hotmail.com>
2021-03-11 21:28:36 -0500

    Fixed #14: refratored generateCSVFiles to FileManager class

--- src/pedigree/Coalescing.java
+++ src/pedigree/Coalescing.java
@@ -1,50 +1,13 @@
 package pedigree;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 
 public class Coalescing {
     
     private HashMap<Double, Integer> aieux; 
     private HashMap<Double, Integer> aieules;
-    
-    public void generateCSVFile(HashMap<Double, Integer> aieux, HashMap<Double, Integer> aieules) throws IOException{
-        String fileName = "test.csv";
-        String filePath = new File("").getAbsolutePath();
-        filePath = filePath.concat("/data/" + fileName);
-        System.out.println(filePath);
         
-        File file = new File(filePath);
-        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-        writer.write("time, size, sex\n");
-        
-        // https://stackoverflow.com/questions/1066589/iterate-through-a-hashmap
-        
-        // add aieules to file
-        Iterator iterMoms = aieules.entrySet().iterator();
-        while(iterMoms.hasNext()){
-            Map.Entry pair = (Map.Entry) iterMoms.next();
-            writer.write(pair.getKey() + "," + pair.getValue() + "," + "F\n");
-            iterMoms.remove(); // avoids concerrentModificationException
-        }
-        
-        // add aieux to file
-        Iterator iterDads = aieux.entrySet().iterator();
-        while(iterDads.hasNext()){
-            Map.Entry pair = (Map.Entry) iterDads.next();
-            writer.write(pair.getKey() + "," + pair.getValue() + "," + "M\n");
-            iterDads.remove(); // avoids concerrentModificationException
-        }
-        
-        writer.close();
-    }
-    
-    public void coalesce(PQ<Sim> population) throws IOException { // TO FIX
+    public void coalesce(PQ<Sim> population) { // TO FIX
         // VERIFY: how can we ensure that lookups are O(1) and not O(n)
         aieux = new HashMap<>(); 
         aieules = new HashMap<>(); 
@@ -70,7 +33,6 @@ public class Coalescing {
         }
 //        System.out.println(aieux.size());
 //        System.out.println(aieules.size());
-//        generateCSVFile(aieux, aieules);
     }
 
     public HashMap<Double, Integer> getAieules() {
---
+++ src/pedigree/FileManager.java
@@ -0,0 +1,45 @@
+package pedigree;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class FileManager {
+    
+    private final String directory; // directory where the files should be saved in
+
+    public FileManager(String directory) {
+        this.directory = directory;
+    }
+    
+    public void generateCSVFileForAncestors(String fileName, HashMap<Double, Integer> aieux, HashMap<Double, Integer> aieules) throws IOException{
+        File file = new File(directory + fileName);
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+        writer.write("time, size, sex\n");
+
+        // https://stackoverflow.com/questions/1066589/iterate-through-a-hashmap
+
+        // add aieules to file
+        Iterator iterMoms = aieules.entrySet().iterator();
+        while(iterMoms.hasNext()){
+            Map.Entry pair = (Map.Entry) iterMoms.next();
+            writer.write(pair.getKey() + "," + pair.getValue() + "," + "F\n");
+            iterMoms.remove(); // avoids concerrentModificationException
+        }
+
+        // add aieux to file
+        Iterator iterDads = aieux.entrySet().iterator();
+        while(iterDads.hasNext()){
+            Map.Entry pair = (Map.Entry) iterDads.next();
+            writer.write(pair.getKey() + "," + pair.getValue() + "," + "M\n");
+            iterDads.remove(); // avoids concerrentModificationException
+        }
+        
+        writer.close();
+    }
+    
+}
