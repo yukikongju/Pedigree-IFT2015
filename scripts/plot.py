3700605e2cc7fc83b5f61b498bb102fea66cdcae
Emulie Chhor <emulie.chhor@hotmail.com>
2021-03-20 16:03:53 -0400

    added plot with both coalescing points and population growth

--- scripts/plot.py
+++ scripts/plot.py
@@ -7,7 +7,7 @@ import pandas as pd
 def plotCoalescing():
     file_path = "../data/coalescing.csv"
     df = pd.read_csv(file_path)
-    df = df.sort_values(by = 'size') # TO FIX: check condition
+    df = df.sort_values(by = 'time') # TO FIX: check condition
     females = df.loc[df['sex'] == 'F'] # df of females
     males = df.loc[df['sex'] == 'M'] # select of males
     xfemales = females['time']
@@ -29,7 +29,31 @@ def plotPopulationGrowth():
     plt.plot(xpopulation, ypopulation)
     plt.show()
 
+def plotCoalescingAndPopulation():
+    file_coalescing = "../data/coalescing.csv"
+    file_population = "../data/population.csv"
+    df = pd.read_csv(file_coalescing)
+    df = df.sort_values(by = 'time') # TO FIX: check condition
+    females = df.loc[df['sex'] == 'F'] # df of females
+    males = df.loc[df['sex'] == 'M'] # select of males
+    xfemales = females['time']
+    yfemales = females['size']
+    xmales = males['time']
+    ymales = males['size']
+    plt.plot(xfemales, yfemales)
+    plt.plot(xmales, ymales)
+    df = pd.read_csv(file_population)
+    df = df.sort_values(by = 'time') # TO FIX: check condition
+    xpopulation = df['time']
+    ypopulation = df['size']
+    #  print(xpopulation)
+    plt.plot(xpopulation, ypopulation)
+    plt.show()
+
+    pass
+
 if __name__ == "__main__":
     plotCoalescing()
     plotPopulationGrowth()
+    plotCoalescingAndPopulation()
 
