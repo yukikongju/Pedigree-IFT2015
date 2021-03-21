281054db5f650c282947c81f9107b79191aa119a
Emulie Chhor <emulie.chhor@hotmail.com>
2021-03-21 16:13:51 -0400

    added scatterplot for coalescence

--- scripts/plot.py
+++ scripts/plot.py
@@ -46,8 +46,44 @@ def plotCoalescingAndPopulation():
     plt.plot(xpopulation, ypopulation)
     plt.show()
 
+def plotScatter():
+    file_coalescing = "../data/coalescing.csv"
+    file_population = "../data/population.csv"
+    df = pd.read_csv(file_coalescing)
+    df = df.sort_values(by = 'time') # TO FIX: check condition
+    females = df.loc[df['sex'] == 'F'] # df of females
+    males = df.loc[df['sex'] == 'M'] # select of males
+    # resizing scatter point
+    sizef = np.full((1,females.shape[0]), 10)
+    sizem = np.full((1,males.shape[0]), 10)
+    #  print(sizef)
+    # plotter scatter plot with line
+    xfemales = females['time']
+    #  xfemales /= 1000
+    yfemales = females['size']
+    yfemales = np.log10(yfemales)
+    xmales = males['time']
+    #  xmales /= 1000
+    ymales = males['size']
+    ymales = np.log10(ymales)
+    plt.scatter(xfemales, yfemales, s = sizef, alpha = 0.6, color = "blue")
+    plt.scatter(xmales, ymales, s = sizem, alpha=0.6, color = "red")
+    plt.plot(xfemales, yfemales, "--b", alpha = 0.6, label ='Aieules')
+    plt.plot(xmales, ymales, "--r", alpha = 0.6, label = 'Aieux')
+    df = pd.read_csv(file_population)
+    df = df.sort_values(by = 'time') # TO FIX: check condition
+    xpopulation = df['time']
+    #  xpopulation /= 1000
+    ypopulation = df['size']
+    ypopulation = np.log10(ypopulation)
+    plt.plot(xpopulation, ypopulation, "green", label = 'Population')
+    plt.xlabel("Years")
+    plt.legend(loc = 'center left')
+    plt.show()
+
 if __name__ == "__main__":
     #  plotCoalescing()
     #  plotPopulationGrowth()
-    plotCoalescingAndPopulation()
+    plotScatter()
+    #  plotCoalescingAndPopulation()
 
