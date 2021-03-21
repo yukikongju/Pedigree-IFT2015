a342dfa16d48af9d6ff9c7b8869eb9ab913a53ac
Emulie Chhor <emulie.chhor@hotmail.com>
2021-03-21 14:43:57 -0400

    executing program from command line

--- scripts/plot.py
+++ scripts/plot.py
@@ -6,9 +6,7 @@ import pandas as pd
 
 def plotCoalescing():
     file_path = "../data/coalescing.csv"
-    #  file_path = "goy.csv"
     df = pd.read_csv(file_path)
-    #  df = df.sort_values(by = 'time') # TO FIX: check condition
     females = df.loc[df['sex'] == 'F'] # df of females
     males = df.loc[df['sex'] == 'M'] # select of males
     xfemales = females['time']
@@ -18,7 +16,6 @@ def plotCoalescing():
     plt.plot(xfemales, yfemales)
     plt.plot(xmales, ymales)
     plt.show()
-    # print(females)
 
 def plotPopulationGrowth():
     file_path = "../data/population.csv"
@@ -26,13 +23,11 @@ def plotPopulationGrowth():
     df = df.sort_values(by = 'time') # TO FIX: check condition
     xpopulation = df['time']
     ypopulation = df['size']
-    #  print(xpopulation)
     plt.plot(xpopulation, ypopulation)
     plt.show()
 
 def plotCoalescingAndPopulation():
     file_coalescing = "../data/coalescing.csv"
-    #  file_coalescing = "goy.csv"
     file_population = "../data/population.csv"
     df = pd.read_csv(file_coalescing)
     df = df.sort_values(by = 'time') # TO FIX: check condition
@@ -48,12 +43,11 @@ def plotCoalescingAndPopulation():
     df = df.sort_values(by = 'time') # TO FIX: check condition
     xpopulation = df['time']
     ypopulation = df['size']
-    #  print(xpopulation)
     plt.plot(xpopulation, ypopulation)
     plt.show()
 
 if __name__ == "__main__":
-    plotCoalescing()
-    plotPopulationGrowth()
+    #  plotCoalescing()
+    #  plotPopulationGrowth()
     plotCoalescingAndPopulation()
 
--- src/pedigree/Main.java
+++ src/pedigree/Main.java
@@ -2,7 +2,6 @@ package pedigree;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.TreeMap;
 
 public class Main {
@@ -11,22 +10,29 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
+        if (args.length != 2) {
+            System.out.println("Invalid numbers of arguments"); // java Simulation <n> <Tmax>
+            System.exit(0);
+        }
+        
+        int n = Integer.parseInt(args[0]);
+        int Tmax = Integer.parseInt(args[1]);
         // Simulate
         Simulation simulation = new Simulation();
         simulation.simulate(1000, 20000); // n>=1000 ; Tmax>=10n
         PQ<Sim> population = simulation.getPopulation();
-        
+
         System.out.println("Taille de population: " + population.size());
-        
+
         // Coalescing
         Coalescing coalescing = new Coalescing();
         coalescing.coalesce(population);
-        
+
         // Fetch HashMap for "Etude Empirique"
         TreeMap<Double, Integer> populationGrowth = simulation.getPopulationGrowth();
         TreeMap<Double, Integer> aieux = coalescing.getAieux();
         TreeMap<Double, Integer> aieules = coalescing.getAieules();
-        
+
         System.out.println("Nombre de lignées paternelles: " + aieux.size());
         System.out.println("Nombre de lignées paternelles: " + aieules.size());
 
@@ -35,7 +41,7 @@ public class Main {
         FileManager manager = new FileManager(downloadPath);
         manager.generateCSVFileForCoalescingPoints("coalescing.csv", aieux, aieules);
         manager.generateCSVFileForPopulationGrowth("population.csv", populationGrowth);
-        
+
         // Generate plot from coalescing: check python script
     }
 
